c08443cd77d644f0adf9ccdc5fa7acb65f00d625
binhang <yuanbinhang@hotmail.com>
2022-01-28 16:34:13 +0100

    foo update

--- pytorch_multi_stream_p2p_test.py
+++ pytorch_multi_stream_p2p_test.py
@@ -1,7 +1,5 @@
 import random
-import torch
 import argparse
-import time
 from nccl_backend import *
 
 
@@ -10,7 +8,23 @@ def test_multi_stream_send_recv(args, device, communicator:NCCLCommunicator):
     torch_even_stream = torch.cuda.Stream(device=device, priority=-1)
     if args.rank == 0:
         start_send_events = [torch.cuda.Event(blocking=False) for _ in range(args.iter)]
-        send_tensors = [torch.full((args.dim,), i, dtype=torch.int, device=device) for i in range(args.iter)]
+        send_tensors = [torch.full((args.dim, args.dim), i, dtype=torch.float, device=device) for i in range(args.iter)]
+        for i in range(args.iter//2):
+            rand_val = random.randint(0, 1)
+            if rand_val == 0:  # insert delay at even event
+                with torch.cuda.default_stream():
+                    torch.cuda.default_stream().record_event(start_send_events[i * 2 + 1])
+                    print("Record event: {}".format(i * 2 + 1))
+                    send_tensors[i * 2] = torch.matmul(send_tensors[i * 2], torch.eye(args.dim, device=device))
+                    torch.cuda.default_stream().record_event(start_send_events[i * 2])
+                    print("Record event: {} insert delay.".format(i * 2))
+            else:
+                with torch.cuda.default_stream():
+                    torch.cuda.default_stream().record_event(start_send_events[i * 2])
+                    print("Record event: {}".format(i * 2))
+                    send_tensors[i * 2 + 1] = torch.matmul(send_tensors[i * 2], torch.eye(args.dim, device=device))
+                    torch.cuda.default_stream().record_event(start_send_events[i * 2 + 1])
+                    print("Record event: {} insert delay.".format(i * 2 + 1))
         for i in range(args.iter):
             if i % 2 == 0:
                 with torch.cuda.stream(torch_even_stream):
@@ -22,6 +36,7 @@ def test_multi_stream_send_recv(args, device, communicator:NCCLCommunicator):
                     cupy_odd_stream = cupy.cuda.ExternalStream(torch_odd_stream.cuda_stream)
                     torch_odd_stream.wait_event(start_send_events[i])
                     communicator.send(send_tensors[i], dst=1, stream=cupy_odd_stream)
+        '''
         print("All send cmd issued.")
         time.sleep(10)
         for i in range(args.iter//2):
@@ -36,18 +51,19 @@ def test_multi_stream_send_recv(args, device, communicator:NCCLCommunicator):
                 print("Record event: {}".format(i * 2 + 1))
                 torch_even_stream.record_event(start_send_events[i*2])
                 print("Record event: {}".format(i * 2))
+        '''
     elif args.rank == 1:
         for i in range(args.iter):
             if i % 2 == 0:
                 with torch.cuda.stream(torch_even_stream):
-                    recv_tensor = torch.zeros((args.dim,), dtype=torch.int, device=device)
+                    recv_tensor = torch.zeros((args.dim, args.dim), dtype=torch.float, device=device)
                     cupy_even_stream = cupy.cuda.ExternalStream(torch_even_stream.cuda_stream)
                     communicator.recv(recv_tensor, src=0, stream=cupy_even_stream)
                     print("====In even stream====")
                     print("Iter {} recv tensor: {} ".format(i, recv_tensor))
             else:
                 with torch.cuda.stream(torch_odd_stream):
-                    recv_tensor = torch.zeros((args.dim,), dtype=torch.int, device=device)
+                    recv_tensor = torch.zeros((args.dim, args.dim), dtype=torch.float, device=device)
                     cupy_odd_stream = cupy.cuda.ExternalStream(torch_odd_stream.cuda_stream)
                     communicator.recv(recv_tensor, src=0, stream=cupy_odd_stream)
                     print("----In odd stream----")
@@ -64,7 +80,7 @@ def main():
                         help='world size (default: 2)')
     parser.add_argument('--rank', type=int, default=0, metavar='R',
                         help='rank for distributed PyTorch')
-    parser.add_argument('--dim', type=int, default=32, metavar='R',
+    parser.add_argument('--dim', type=int, default=1000, metavar='R',
                         help='size of the tensor to be sent.') # this is an approximated size of a macro-bench
     parser.add_argument('--use-cuda', default=False, type=lambda x: (str(x).lower() == 'true'),
                         help='if this is set to True, will use cuda to train')
