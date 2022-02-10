aa9a790d4b9222079247c593fa8f99bb26c87921
binhang <yuanbinhang@hotmail.com>
2022-02-10 23:37:26 +0100

    added some implementation

---
+++ pytorch_collective_test.py
@@ -0,0 +1,143 @@
+import random
+import torch
+import argparse
+import time
+import torch.distributed as dist
+from nccl_backend import NCCLCommunicator
+
+
+def data_size_mb2dim(mb:int):
+    return mb // 4 * 1024 * 1024
+
+
+def test_allreduce(args, device, communicator:NCCLCommunicator):
+    print("<==== Test AllReduce ====>")
+    dim = data_size_mb2dim(args.dim_mb)
+    tensor = torch.arange(dim, dtype=torch.float32, device=device)
+    if args.use_cuda:
+        torch.cuda.synchronize()
+
+    start_time = time.time()
+    communicator.all_reduce(tensor)
+
+    if args.use_cuda:
+        torch.cuda.synchronize()
+    end_time = time.time()
+    total_time = end_time - start_time
+    print('AllReduce of tensor <', args.dim_mb, "> MB takes ", total_time, "seconds.")
+    return total_time
+
+
+def test_broadcast(args, device, communicator:NCCLCommunicator):
+    print("<==== Test Broadcast ====>")
+    dim = data_size_mb2dim(args.dim_mb)
+    if args.rank == 0:
+        tensor = torch.arange(dim, dtype=torch.float32, device=device)
+    else:
+        tensor = torch.zeros(dim, dtype=torch.float32, device=device)
+
+    if args.use_cuda:
+        torch.cuda.synchronize()
+
+    start_time = time.time()
+    communicator.broadcast(tensor, src=0)
+
+    if args.use_cuda:
+        torch.cuda.synchronize()
+    end_time = time.time()
+    total_time = end_time - start_time
+    print('Broadcast of tensor <', args.dim_mb, "> MB takes ", total_time, "seconds.")
+    return total_time
+
+
+def test_reduce(args, device, communicator: NCCLCommunicator):
+    print("<==== Test Reduce ====>")
+    dim = data_size_mb2dim(args.dim_mb)
+    if args.rank == 0:
+        tensor = torch.zeros(dim, dtype=torch.float32, device=device)
+    else:
+        tensor = torch.arange(dim, dtype=torch.float32, device=device)
+
+    if args.use_cuda:
+        torch.cuda.synchronize()
+
+    start_time = time.time()
+    communicator.reduce(tensor, dst=0)
+
+    if args.use_cuda:
+        torch.cuda.synchronize()
+    end_time = time.time()
+    total_time = end_time - start_time
+    print('Reduce of tensor <', args.dim_mb, "> MB takes ", total_time, "seconds.")
+    return total_time
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Test PyTorch Distributed')
+    parser.add_argument('--dist-backend', type=str, default='gloo', metavar='S',
+                        help='PyTorch backend type')
+    parser.add_argument('--dist-url', type=str, default='tcp://127.0.0.1:9000', metavar='S',
+                        help='master ip for distributed PyTorch')
+    parser.add_argument('--world-size', type=int, default=2, metavar='D',
+                        help='world size (default: 2)')
+    parser.add_argument('--rank', type=int, default=0, metavar='R',
+                        help='rank for distributed PyTorch')
+    parser.add_argument('--dim_mb', type=int, default=128, metavar='R',
+                        help='size of the tensor to be sent. (in MB)')
+    parser.add_argument('--use-cuda', default=False, type=lambda x: (str(x).lower() == 'true'),
+                        help='if this is set to True, will use cuda to train')
+    parser.add_argument('--cuda-id', type=int, default=0, metavar='N',
+                        help='cuda index, if the instance has multiple GPUs.')
+    parser.add_argument('--iter', type=int, default=10, metavar='R',
+                        help='number of iterations for benchmark.')
+    args = parser.parse_args()
+
+    if args.use_cuda:
+        assert (torch.cuda.is_available())
+        device = torch.device('cuda', args.cuda_id)
+    else:
+        device = torch.device('cpu')
+    if args.dist_backend == 'cupy_nccl':
+        communicator = NCCLCommunicator(rank=args.rank, intra_gpu_rank=args.cuda_id,
+                                        world_size=args.world_size, master_ip=args.dist_url)
+    else:
+        dist.init_process_group(backend=args.dist_backend, init_method=args.dist_url,
+                                rank=args.rank, world_size=args.world_size)
+        communicator = dist
+
+    allreduce_time = 0
+    for i in range(args.iter + 1):
+        dist.barrier()
+        if i == 0:
+            test_allreduce(args, device, communicator)
+        else:
+            allreduce_time += test_allreduce(args, device, communicator)
+        time.sleep(1)
+    allreduce_time /= args.iter
+    print("<=====Averaged AllReduce time: ", allreduce_time * 1000, "ms.=====>")
+
+    broadcast_time = 0
+    for i in range(args.iter + 1):
+        dist.barrier()
+        if i == 0:
+            test_broadcast(args, device, communicator)
+        else:
+            broadcast_time += test_broadcast(args, device, communicator)
+        time.sleep(1)
+    broadcast_time /= args.iter
+    print("<=====Averaged Broadcast time: ", broadcast_time * 1000, "ms.=====>")
+
+    reduce_time = 0
+    for i in range(args.iter + 1):
+        dist.barrier()
+        if i == 0:
+            test_reduce(args, device, communicator)
+        else:
+            reduce_time += test_reduce(args, device, communicator)
+        time.sleep(1)
+    reduce_time /= args.iter
+    print("<=====Averaged AllReduce time: ", reduce_time * 1000, "ms.=====>")
+
+
+if __name__ == '__main__':
+    main()
---
+++ results/broadcast.md
@@ -0,0 +1,34 @@
+## Broadcast
+
+#### 4 node:
+| Network setting             | Gloo-128 MB | NCCL-128 MB | Gloo-256 MB | NCCL-256 MB |
+|-----------------------------|-------------|-------------|-----|-----|
+| AWS default                 | ms          | ms          |  ms |  ms |
+| delay 1ms  bandwidth 5Gbps  | ms          | ms          |  ms |  ms |
+| delay 5ms  bandwidth 2Gbps  | ms          | ms          |  ms |  ms |
+| delay 10ms  bandwidth 1Gbps | ms          | ms          |  ms |  ms |
+
+#### 8 node:
+| Network setting             | Gloo-128 MB | NCCL-128 MB | Gloo-256 MB | NCCL-256 MB |
+|-----------------------------|-------------|-------------|-----|-----|
+| AWS default                 | ms          | ms          |  ms |  ms |
+| delay 1ms  bandwidth 5Gbps  | ms          | ms          |  ms |  ms |
+| delay 5ms  bandwidth 2Gbps  | ms          | ms          |  ms |  ms |
+| delay 10ms  bandwidth 1Gbps | ms          | ms          |  ms |  ms |
+
+#### 16 node:
+| Network setting             | Gloo-128 MB | NCCL-128 MB | Gloo-256 MB | NCCL-256 MB |
+|-----------------------------|-------------|-------------|-----|-----|
+| AWS default                 | ms          | ms          |  ms |  ms |
+| delay 1ms  bandwidth 5Gbps  | ms          | ms          |  ms |  ms |
+| delay 5ms  bandwidth 2Gbps  | ms          | ms          |  ms |  ms |
+| delay 10ms  bandwidth 1Gbps | ms          | ms          |  ms |  ms |
+
+
+#### 32 node:
+| Network setting             | Gloo-128 MB | NCCL-128 MB | Gloo-256 MB | NCCL-256 MB |
+|-----------------------------|-------------|-------------|-----|-----|
+| AWS default                 | ms          | ms          |  ms |  ms |
+| delay 1ms  bandwidth 5Gbps  | ms          | ms          |  ms |  ms |
+| delay 5ms  bandwidth 2Gbps  | ms          | ms          |  ms |  ms |
+| delay 10ms  bandwidth 1Gbps | ms          | ms          |  ms |  ms |
