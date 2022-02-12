e9482ca9ab5f20f34c229b8b0b4a4d1a69878d43
binhang <yuanbinhang@hotmail.com>
2022-02-12 18:58:44 +0100

    foo update

--- run_benchmark_script/local_python_script_wo_tc.sh
+++ run_benchmark_script/local_python_script_wo_tc.sh
@@ -10,6 +10,6 @@ RANK=$2
 timestamp=$(date +%Y_%m_%d_%H_%M)
 
 # The private IP of Rank-0 node should be manually updated in each run.
-python pytorch_collective_test.py --dist-url tcp://172.31.45.33:9000 --dist-backend gloo --world-size "$WORLD_SIZE" --rank "$RANK" >>  "./logs/${timestamp}_gloo_default.log"
+python pytorch_collective_test.py --dist-url tcp://172.31.45.33:9000 --dist-backend gloo --use-cuda False --world-size "$WORLD_SIZE" --rank "$RANK" >>  "./logs/${timestamp}_gloo_default.log"
 
-python pytorch_collective_test.py --dist-url tcp://172.31.45.33:9000 --dist-backend cupy_nccl --world-size "$WORLD_SIZE" --rank "$RANK" >>  "./logs/${timestamp}_cupyNccl_default.log"
+python pytorch_collective_test.py --dist-url tcp://172.31.45.33:9000 --dist-backend cupy_nccl --use-cuda True --world-size "$WORLD_SIZE" --rank "$RANK" >>  "./logs/${timestamp}_cupyNccl_default.log"
