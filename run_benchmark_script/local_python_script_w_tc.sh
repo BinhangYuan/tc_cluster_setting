4e725cbbdc278ed8a9114a6cb6853185b13e4bb9
binhang <yuanbinhang@hotmail.com>
2022-02-12 18:55:15 +0100

    foo update

--- run_benchmark_script/local_python_script_w_tc.sh
+++ run_benchmark_script/local_python_script_w_tc.sh
@@ -14,8 +14,8 @@ timestamp=$(date +%Y_%m_%d_%H_%M)
 sh ./tc_script/tc_netem_delay_bandwdith.sh $DELAY_MS $RATE_GBIT >>  "./log/${timestamp}.log"
 
 # The private IP of Rank-0 node should be manually updated in each run.
-python pytorch_collective_test.py --dist-url tcp://172.31.45.33:9000 --dist-backend gloo --world-size "$WORLD_SIZE" --rank "$RANK" >>  "./logs/${timestamp}_gloo_d${DELAY_MS}b${RATE_GBIT}.log"
+python pytorch_collective_test.py --dist-url tcp://172.31.45.33:9000 --dist-backend gloo --use-cuda False --world-size "$WORLD_SIZE" --rank "$RANK" >>  "./logs/${timestamp}_gloo_d${DELAY_MS}b${RATE_GBIT}.log"
 
-python pytorch_collective_test.py --dist-url tcp://172.31.45.33:9000 --dist-backend cupy_nccl --world-size "$WORLD_SIZE" --rank "$RANK" >>  "./logs/${timestamp}_cupy_nccl_d${DELAY_MS}b${RATE_GBIT}.log"
+python pytorch_collective_test.py --dist-url tcp://172.31.45.33:9000 --dist-backend cupy_nccl --use-cuda True --world-size "$WORLD_SIZE" --rank "$RANK" >>  "./logs/${timestamp}_cupy_nccl_d${DELAY_MS}b${RATE_GBIT}.log"
 
 sh ./tc_script/tc_clear.sh
