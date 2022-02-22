6f92411d9d4b59674c7500d84b1becfb6a92c833
binhang <yuanbinhang@hotmail.com>
2022-02-22 18:05:58 +0100

    updated some scripts.

--- test_cross_region_nccl_init.py
+++ test_cross_region_nccl_init.py
@@ -8,7 +8,7 @@ def main():
     parser = argparse.ArgumentParser(description='Test Cross region NCCL init')
     parser.add_argument('--rank', type=int, default=0, metavar='R',
                         help='rank for distributed PyTorch')
-    parser.add_argument('--cuda-id-str', type=str, default='?', metavar='S',
+    parser.add_argument('--cuda-id-str', type=bytes, default='?', metavar='S',
                         help='Magic cuda-id-string')
     args = parser.parse_args()
 
@@ -18,11 +18,17 @@ def main():
         cuda_id_str = np.array(comm_id).tobytes()
         print(cuda_id_str)
         nccl_comm = cupy.cuda.nccl.NcclCommunicator(2, comm_id, 0)
+        print("Initialize NCCL succeed! - Rank 0")
     elif args.rank == 1:
         comm_id = tuple(np.frombuffer(args.cuda_id_str, dtype=int))
         nccl_comm = cupy.cuda.nccl.NcclCommunicator(2, comm_id, 1)
+        print("Initialize NCCL succeed! - Rank 1")
 
     tensor = torch.ones(1024, dtype=torch.float32, device='cuda:0')
     nccl_comm.allReduce(tensor.data_ptr(), tensor.data_ptr(), torch.numel(tensor), cupy.cuda.nccl.NCCL_FLOAT32,
                         cupy.cuda.nccl.NCCL_SUM, cupy.cuda.Stream.null)
     print(tensor)
+
+
+if __name__ == '__main__':
+    main()
