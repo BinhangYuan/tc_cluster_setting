8a7003b320b85372b0ac71cff8f4e2cf91a53ccc
binhang <yuanbinhang@hotmail.com>
2022-04-19 13:59:44 +0200

    foo

--- pytorch_debug_allreduce_opt.py
+++ pytorch_debug_allreduce_opt.py
@@ -32,7 +32,7 @@ def test_paradigm_sharded_ps_correct(args, device, communicator: NCCLCommunicato
     buffer = [torch.zeros(chunk_size) for _ in range(args.world_size)]
     print("<==== Cast 1 ====>")
     print("Before sync:", tensor)
-    communicator.all_to_all(tensor, buffer)
+    communicator.all_to_all(tensors, buffer)
     print(buffer[0])
     for i in range(1, args.world_size):
         print(buffer[i])
